a532205fb21d586b815ecfc6f3b910469aad0e2c
Arka das <165567289+1arka02@users.noreply.github.com>
2024-07-10 21:01:08 +0530

    Add files via upload

---
+++ build/precache-manifest.8c207b9ddf8cde489683d84e5fec0b2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0744d2cef1b7cf27980c41a8e4224c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dec6a60c12c08e90022a",
+    "url": "/static/css/main.ff90eb11.chunk.css"
+  },
+  {
+    "revision": "a7c3058bd7eeb7de2bcf",
+    "url": "/static/js/2.fe5abab2.chunk.js"
+  },
+  {
+    "revision": "ea862b4d7291d9d99556",
+    "url": "/static/js/3.bd0f349a.chunk.js"
+  },
+  {
+    "revision": "dec6a60c12c08e90022a",
+    "url": "/static/js/main.0716dc66.chunk.js"
+  },
+  {
+    "revision": "cebf86d23c5f069e6979",
+    "url": "/static/js/runtime~main.3388e78b.js"
+  }
+]);
